248175a6203788a17787d9ce0b0400b20e3296be
theJPster <github@thejpster.org.uk>
2013-11-13 11:06:32 +0000

    Added more logging.

--- svn_revision/templatetags/__init__.py
+++ svn_revision/templatetags/__init__.py
@@ -2,6 +2,8 @@ def get_revision():
     from subprocess import check_output, PIPE
     import re
     import os
+    import logging
+    log = logging.getLogger("svn_revision")
 
     result = "Versioning Unavailable"
     try:
@@ -10,9 +12,9 @@ def get_revision():
         m = re.match('(\d+:?\d*[MS])$', outS)
         if m and m.group(1):
             result = 'r' + m.group(1)
+        else:
+            log.warn("Didn't like SVN output %s", outS)
     except:
-        import logging
-        log = logging.getLogger("svn_revision")
         log.exception("Failed to call SVN")
 
     return result
