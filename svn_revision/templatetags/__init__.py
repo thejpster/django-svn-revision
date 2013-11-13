e4485faa8d9bb28d8b723609d11a059c480ac969
theJPster <github@thejpster.org.uk>
2013-11-13 10:51:06 +0000

    Changed to use subprocess.check_output (os.popen3 is deprecated) Return full xxx:yyyM revision string (it's important to know when your SVN checkout has been modified) Log on error.

--- svn_revision/templatetags/__init__.py
+++ svn_revision/templatetags/__init__.py
@@ -1,19 +1,20 @@
 def get_revision():
-    from os import popen3
-    import re, os
+    from subprocess import check_output, PIPE
+    import re
+    import os
 
+    result = "Versioning Unavailable"
     try:
-        stIO = popen3('svnversion '+os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../../"))
-        outS = stIO[1].read().strip()
-        m = re.match(':?(\d*).*[MS]?$', outS);
-        errS = stIO[2].read().strip()
-        for f in stIO: f.close()
-
-        if errS:
-            return errS
-        if m:
-            return m.group(1) or ' '
+        path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../../")
+        outS = check_output([ 'svnversion', path ])
+        m = re.match('(\d+:?\d*[MS])$', outS)
+        if m and m.group(1):
+            result = 'r' + m.group(1)
     except:
-        return 'Versioning Unavailable'
+        import logging
+        log = logging.getLogger("svn_revision")
+        log.exception("Failed to call SVN")
+
+    return result
 
 REVISION = get_revision()
